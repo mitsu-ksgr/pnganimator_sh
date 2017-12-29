305ab9f521dad034e3618d4399fb6f97da316b19
mitsu-ksgr <mitsu.naka.dev@gmail.com>
2017-12-29 12:15:59 +0900

    add files

---
+++ lib.sh
@@ -0,0 +1,163 @@
+#
+# Common functionality
+#
+
+#===============================================================================
+#   Common Functions
+#===============================================================================
+
+#
+# shell outputs
+#
+warn() { echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" 1>&2; }
+err() { echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" 1>&2; }
+die() { err "$@"; exit 1; }
+
+#
+# Logging
+#
+LOG_LEVEL=0 # 0:info/err, 1:verbose, 2:debug
+logv() { [[ LOG_LEVEL -ge 1 ]] && echo $@; return 0; }
+logd() { [[ LOG_LEVEL -ge 2 ]] && echo $@; return 0; }
+
+#--------------------------------------
+# Make Animation GIF.
+# Arguments:
+#   1. targets
+#   2. dst
+#   3. delay
+make_ani_gif() {
+  convert -delay $3 \
+          -loop 0 \
+          -dispose previous \
+          "$1" "$2"
+}
+
+#--------------------------------------
+# Optimize GIF animation.
+# Arguments:
+#   1. target file path
+optimize() {
+  convert $1 -layers Optimize $1
+  logd "optimize: optimized $1"
+}
+
+#--------------------------------------
+# Resize & Background transparent of PNG Image.
+# TODO: testing.
+# Arguments:
+#   1. target file path
+#   3. size (length of one side)
+resize() {
+  convert "$1" \
+          -resize "${2}x${2}" \
+          -background none \
+          -fill '#00000000' \
+          -transparent white \
+          -fuzz 10% \
+          -draw 'matte 1,1 floodfill' \
+          "$1"
+  logd "resize: resized $1"
+}
+# @Note
+# 1. copy input file to tmp
+# 2. resize it
+# 3. make ani-gif with an resized image.
+
+
+#===============================================================================
+#   Rotate Functions
+#===============================================================================
+
+#--------------------------------------
+# Rotate Image
+# Arguments:
+#   1. src
+#   2. dst
+#   3. angle
+make_rotate_image() {
+  local src=$1
+  local dst=$2
+  local angle=$3
+  # TODO: extent size...
+  convert -rotate "${angle}" \
+          -gravity center \
+          -extent 128x128 \
+          -background none \
+          "${src}" "${dst}"
+  logd "make_rotate_image: ${dst} --- src=${src}"
+}
+
+#--------------------------------------
+# Generate rotate animation-gif.
+# Arguments:
+#   1. src
+#   2. dst
+#   3. tmp
+#   4. speed
+#   5~n. angles
+_rotate() {
+  local src=$1
+  local dst=$2
+  local tmp=$3
+  local speed=$4
+  shift 4
+  local angles=($@)
+
+  for i in ${angles[@]}; do
+    make_rotate_image "${src}" "${tmp}/${i}.png" $i
+  done
+  make_ani_gif "${tmp}/*.png" "${dst}" $speed
+  logd "_rotate: generated to ${dst}"
+
+  return 0
+}
+
+#--------------------------------------
+# Generate rotate Animation GIF.
+# Arguments:
+#   1. src_file
+#   2. dst_file_name_base
+#   3. work_dir - path to temporary dir.
+#   4. frames - animation frames.
+#   5. delay - animation frame delay.
+#   6. optimize_flag
+# Returns:
+#   None
+rotate() {
+  local src_file=$1
+  local dst_filename_base=$2
+  local work_dir=$3
+  local angle=`expr 360 / $4`
+  local delay=$5
+  local flag_optimize=$6
+
+  logd "---------- RotateFunction ----------"
+  logd "## Arguments"
+  logd "src_file: ${src_file}"
+  logd "dst_filename_base: ${dst_filename_base}"
+  logd "work_dir: ${work_dir}"
+  logd "frames: $4 --> angle=${angle}"
+  logd "delay: ${delay}"
+  logd
+
+  # Clockwise
+  mkdir "${work_dir}/cwise"
+  _rotate "${src_file}" \
+          "${dst_filename_base}_cwise.gif" \
+          "${work_dir}/cwise" \
+          $delay \
+          `seq -w ${angle} ${angle} 360`
+  [[ "${flag_optimize}" = 'true' ]] && optimize "${dst_filename_base}_cwise.gif"
+
+  # Counter Clockwise
+  mkdir "${work_dir}/ccwise"
+  _rotate "${src_file}" \
+          "${dst_filename_base}_ccwise.gif" \
+          "${work_dir}/ccwise" \
+          $delay \
+          `seq -w -${angle} -${angle} -360`
+  [[ "${flag_optimize}" = 'true' ]] && optimize "${dst_filename_base}_ccwise.gif"
+
+  return 0
+}
---
+++ pnganimator.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+#
+# Generate a simple animation GIF from png image.
+#
+
+set -e
+
+#
+# Constant Values
+#
+readonly PNGANIMATOR_DIR=`dirname $0`
+readonly PNGANIMATOR_NAME=`basename $0`
+
+# Default option values
+readonly DEFAULT_OPT_FRAME=8 #45 # 30
+readonly DEFAULT_OPT_DELAY=8  # 7
+
+
+#
+# Support Functions
+#
+usage() {
+    cat << __EOS__
+Usage: $PNGANIMATOR_NAME [-s SPEED] [-f FRAME] [-d DEST_FILE] [-o] [-v] [-x] anim_mode input_file.png
+
+Description:
+    Generate a simple animation GIF from png image.
+
+Animation Mode:
+    rotate  - Generate animation GIF in wich the image ratates.
+    move    - Generate animation GIF in wich the image moves. (TODO)
+
+Options:
+    -s  Animation Speed. Default $OPT_FRAME_DEFAULT.
+    -f  Animation Frames. Default $OPT_DELAY_DEFAULT.
+    -d  Destination File Name Base.
+    -o  Run optimize.
+    -v  Verbose Mode.
+    -x  Debug Mode.
+    -h  Show help.
+
+__EOS__
+}
+
+
+#
+# Prepare temporary directory
+#
+unset tmp_dir
+
+on_exit() {
+  [[ -n "${tmp_dir}" ]] && rm -rf "${tmp_dir}";
+}
+trap on_exit EXIT
+trap 'trap - EXIT; on_exit; exit -1' INT PIPE TERM
+
+readonly tmp_dir=`mktemp -d "/tmp/${PNGANIMATOR_NAME}.tmp.XXXXXX"`
+
+
+#
+# PngAnimator Main
+#
+main() {
+  # Load lib
+  . "$PNGANIMATOR_DIR/lib.sh"
+
+  # Args & Options
+  local anim_mode=''
+  local output_filename_base=''
+  local flag_optimize='false'
+  local opt_frame=$DEFAULT_OPT_FRAME
+  local opt_delay=$DEFAULT_OPT_DELAY
+
+  while getopts d:of:s:vxh flag; do
+    case "${flag}" in
+      # Common options
+      d ) output_filename_base="${OPTARG}" ;;
+      o ) flag_optimize='true' ;;
+
+      # Animation option
+      f ) opt_frame="${OPTARG}" ;;
+      s ) opt_delay="${OPTARG}" ;;
+
+      # Debugging
+      v ) LOG_LEVEL=1 ;;
+      x ) LOG_LEVEL=2 ;;
+
+      # Helps / Error
+      h ) usage ; exit 1 ;;
+      * ) err "error: Unexpected option '${flag}'" ; usage ; exit 1 ;;
+    esac
+  done
+  readonly LOG_LEVEL
+
+  shift `expr $OPTIND - 1`
+  readonly anim_mode=$1
+  readonly input_file_path=$2
+
+  if [ -z "${input_file_path}" ]; then
+    err "error: no input file."
+    usage
+    exit 1
+  fi
+
+  # If not specified, use input file name without extension.
+  if [ -z "${output_filename_base}" ]; then
+    local input_filename=`basename ${input_file_path}`
+    output_filename_base=${input_filename%.*}
+  fi
+  readonly output_filename_base
+
+  readonly flag_optimize
+  readonly opt_frame
+  readonly opt_delay
+
+  logd "---------- Args & Options ----------"
+  logd "## Global Variables"
+  logd "PngAnimator Dir: ${PNGANIMATOR_DIR}"
+  logd "PngAnimator Name: ${PNGANIMATOR_NAME}"
+  logd "Log Level: ${LOG_LEVEL}"
+  logd
+  logd "## Input / Output"
+  logd "Input File Path: ${input_file_path}"
+  logd "Output File Name Base: ${output_filename_base}"
+  logd
+  logd "## Common Options"
+  logd "Anim Mode: ${anim_mode}"
+  logd "Optimize Flag: ${flag_optimize}"
+  logd
+  logd "## Animation Options"
+  logd "Frame: ${opt_frame}"
+  logd "Speed: ${opt_delay}"
+  logd
+  logd "## Temporary Dirs"
+  logd "TmpDir: ${tmp_dir}"
+  logd
+
+  #---------------------------------------------------------
+  case "${anim_mode}" in
+    rotate )
+      rotate "${input_file_path}" \
+             "${output_filename_base}" \
+             "${tmp_dir}" \
+             $opt_frame $opt_delay "${flag_optimize}"
+      ;;
+
+    move )
+      echo "todo"
+      ;;
+
+    * )
+      err "error: Unexpected Animation Mode Specified '${mode}'"
+      exit 1
+      ;;
+  esac
+
+  exit 0
+}
+
+main $@
